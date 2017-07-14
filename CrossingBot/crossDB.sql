59d0d1d60f9a8627c4610d9e04af4657ffeb5705
MuteBard <csevere@brandeis.edu>
2017-07-13 23:56:23 -0400

    planning out database

---
+++ CrossingBot/crossDB.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE viewer()
+--id
+--contains net
+--contain pole
+--bells
+--level
+--turnips
+--counter
+CREATE TABLE bugs()
+--id
+--month attr
+--bells
+CREATE TABLE fish()
+--id
+--month attr
+--bells
+CREATE TABLE trees()
+--id
+--fruit
+--money
+--bees
