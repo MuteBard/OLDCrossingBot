ce0f1a01804101e13a5fb760aaccd16235dfb41e
MuteBard <csevere@brandeis.edu>
2017-07-16 13:07:48 -0400

    bot can whisper

--- CrossingBot/crossServer.js
+++ CrossingBot/crossServer.js
@@ -15,11 +15,28 @@ var options = {
   },
   channels: ["MidnightFreeze", "MuteBard"]
 };
-
+// --------------
 var client = new tmi.client(options);
-client.connect()
+var client2 = new tmi.client(options);
 
-client.on('connected', (address, port) => {
-  console.log("Address: "+address+ "\nPort: "+ port);
-  client.action("MuteBard", "NotLikeThis");
+client.connect()
+client.on('join', (channel,username) => console.log(username))
+client.on('chat', (channel, username, message, self) =>{
+  if(message.charAt(0) == "!"){
+    client.action("MuteBard", `${username["display-name"]} Hello!`)
+  }
+})
+client.on('connected', (address, port) => client.action("MuteBard", "NotLikeThis"));
+
+client2.connect().then((data) => {
+    // Change schmoopiie to your username (not your bot as you can't whisper to yourself)
+    client2.whisper("MuteBard", "I am Alive Too");
+}).catch((err) => {
+    console.log(err);
+});
+
+// Send a whisper to your bot to trigger this event..
+client2.on("whisper", function (user, message) {
+    console.log(user);
+    console.log(message);
 });
