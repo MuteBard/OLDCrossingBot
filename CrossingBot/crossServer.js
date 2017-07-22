7ef1599cb6efbe255341cb949582c2dd952c27b2
MuteBard <csevere@brandeis.edu>
2017-07-21 22:54:38 -0400

    changed tables cols and implemented pocket adding

--- CrossingBot/crossServer.js
+++ CrossingBot/crossServer.js
@@ -32,7 +32,7 @@ Database.prototype.setMonth = function(number){
   crossbase.monthNumber = number;
 }
 Database.prototype.setAnimals = function(cb){
-  db.any(`select * from animal`)
+  db.any(`SELECT * FROM animal`)
     .then(data => {
       cb(data)
     })
@@ -52,9 +52,9 @@ Database.prototype.setEcoSystem = function(cb){
     db.none(`DELETE FROM ecosystem`)
     recievedData.forEach(elem => {
       db.none(`
-          INSERT INTO ecosystem (species, name, bells, months, rarity)
-          VALUES ($1,$2,$3,$4,$5)`,
-          [elem.species, elem.name, elem.bells, elem.months, elem.rarity])
+          INSERT INTO ecosystem (ida ,species, name, bells, months, rarity)
+          VALUES ($1,$2,$3,$4,$5,$6)`,
+          [elem.ida, elem.species, elem.name, elem.bells, elem.months, elem.rarity])
       })
     cb(`ECOSYSTEM ${crossbase.monthChar}`)
   })
@@ -69,8 +69,13 @@ function selectRarity(){
   else                   return 1
 }
 
+function selectItem(size){
+  var num = Math.floor((Math.random() * size) + 1);
+  return num
+}
+
 crossbase = new Database()
-crossbase.setMonth(7)
+crossbase.setMonth(7); //Jan = 0, Feb = 1 ..... Dec = 11
 crossbase.setEcoSystem(function cb(recievedData){console.log(recievedData)});
 
 //
@@ -85,20 +90,53 @@ crossbase.setEcoSystem(function cb(recievedData){console.log(recievedData)});
 //
 //
 // // client.on('join', (channel,username) => console.log(username))
-//
+
+//Works but be sure to protect against duplicates later
 client.on('chat', (channel, username, message, self) => {
-  console.log("DATABASE INSERTION START A")
   if(message == "!start"){
-    console.log("DATABASE INSERTION  B")
     db.none(`
         INSERT INTO viewer (username, net, pole, level, bells, turnips)
         VALUES ($1, $2, $3, $4, $5, $6)`,
         [username["display-name"],1, 1, 1, 0, 0])
+      .then(() => {
+        client.action(`${username["display-name"]}`, `Welcome ${username["display-name"]}! you have joined the town VoHiYo  `)
+        console.log(`${username["display-name"]} ADDED`)
+      })
     }
-  console.log("DATABASE INSERTION START C")
-  client.action(`${username["display-name"]}`, `Welcome ${username["display-name"]}! you have joined the town VoHiYo  `)
-  console.log(`${username["display-name"]}`)
 });
+
+//arrays are specialluzed for order
+//dictionaries are optimized for searching
+
+client.on('chat', (channel, username, message, self) => {
+
+  if(message == "!use.bugnet" || message == "!use.fishpole"){
+    var species
+    if(message.slice(5,6) == "b") species = "bug"
+    else species = "fish"
+    var rare = selectRarity()
+    db.any(`SELECT * FROM ECOSYSTEM WHERE rarity = $1 AND species = $2`, rare, species)
+      .then(data => {
+       var itemIndex = selectItem(data.length)
+       return data[itemIndex]
+    }).then( data => {
+       db.none(`INSERT INTO pocket (username, aid)
+       VALUES ($1, $2)`,
+       `${username["display-name"]}`, data.ida)
+       console.log(`${username["display-name"]} NET USED`)
+       return data
+    }).then(data => {
+       client.action(`${username["display-name"]}`, `${username["display-name"]} you have caught a ${data.species}, the ${data.name}`)
+       console.log(`${username["display-name"]} ${data.name} POCKETED`)
+    }
+
+    );
+  }
+});
+
+// SELECT * FROM ECOSYSTEM WHERE rarity = 2 AND species = "bug"
+
+
 //
 //
 // //Whispers
